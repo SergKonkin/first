83ab086892c9e1278cae4fda3627f1b8a424d1b7
Sergey Konkin <sk_rb@mail.ru>
2021-08-17 16:09:07 +0300

    init 123

--- src/main/java/ru/mail/sk/first/Same.java
+++ src/main/java/ru/mail/sk/first/Same.java
@@ -1,4 +1,16 @@
 package ru.mail.sk.first;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Same {
+
+    @Getter
+    @Setter
+    private Long id;
+
+    public Same(Long id) {
+        this.id = id;
+    }
+
 }
